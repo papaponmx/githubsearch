fa8104f033094c70d58031db6d48384ea76969db
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-16 12:18:59 -0500

    Initial markup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReposService } from './repos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'github-search';
+export class AppComponent implements OnInit {
+  reposData: Array<any>;
+  title = 'Repo Search App';
+  imgUrl = '/src/assets/sharp-cloud-24px.svg'
+  altText = 'Waddup'
+
+  constructor(
+    private reposService: ReposService,
+  ) {}
+
+  ngOnInit() {
+  }
+
+  onSearchRepos() {
+    this.reposService.getRepos('angular')
+      .subscribe(res => console.log(res));
+  }
+
 }
