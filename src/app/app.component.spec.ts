804e4746ddae79298cb0893ab7db5092a4841d5e
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-16 19:43:24 -0500

    Added tests for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,22 @@
+import { ReposListComponent } from './repos-list/repos-list.component';
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ReposListComponent,
+      ],
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
       ],
     }).compileComponents();
   }));
@@ -13,15 +25,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'github-search'`, async(() => {
+  it(`should have as title 'Github Repos'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('github-search');
+    expect(app.title).toEqual('Github Repos');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to github-search!');
+    expect(compiled.querySelector('h1').textContent).toContain('Github Repos');
   }));
 });
