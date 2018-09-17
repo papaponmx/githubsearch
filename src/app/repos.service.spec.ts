a22f318178287f7cf88de395d64d3534714254eb
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-16 20:17:37 -0500

    Added basic test for reposService.getRepos

--- src/app/repos.service.spec.ts
+++ src/app/repos.service.spec.ts
@@ -1,12 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { ReposService } from './repos.service';
 
 describe('ReposService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [ReposService],
+  }));
 
   it('should be created', () => {
     const service: ReposService = TestBed.get(ReposService);
     expect(service).toBeTruthy();
   });
+
+  it('Should getRepos', inject(
+     [HttpTestingController, ReposService],
+     (httpMock: HttpTestingController,
+      reposService: ReposService) => {
+        reposService.getRepos('angular')
+         .subscribe((event: HttpEvent<any>) => {
+          switch (event.type) {
+            case HttpEventType.Response:
+              expect(event.body).toBeDefined();
+          }
+        });
+     }
+  ));
 });
