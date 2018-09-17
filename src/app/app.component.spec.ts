63c2cd7730a3657a544268e4665dc0cf8c2d5ef0
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-16 19:55:11 -0500

    Added more tests for inputs.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,20 +20,45 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'Github Repos'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Github Repos');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Github Repos');
+  }));
+
+  it('Should render a label', async(() => {
+    const labelText = 'Search something interesting like Guess.js, or Puppeteer';
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('label').textContent).toContain(labelText);
+  }));
+
+  it('Should render an input', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input')).toBeTruthy();
+  }));
+
+  it('Should NOT render a button initially', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button')).toBeFalsy();
   }));
 });
